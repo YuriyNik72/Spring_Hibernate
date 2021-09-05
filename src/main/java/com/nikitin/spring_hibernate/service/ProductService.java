f1007a6cfdcf08d746514d467967a9656611580a
Yuriy <yurgennik@gmail.com>
2021-09-05 19:54:24 +0500

    Fix1

--- src/main/java/com/nikitin/spring_hibernate/service/ProductService.java
+++ src/main/java/com/nikitin/spring_hibernate/service/ProductService.java
@@ -1,10 +1,9 @@
 package com.nikitin.spring_hibernate.service;
 
 import com.nikitin.spring_hibernate.dao.ProductDAO;
+import com.nikitin.spring_hibernate.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import com.nikitin.spring_hibernate.entity.Product;
-import com.nikitin.spring_hibernate.repositories.ProductRepository;
 
 import java.util.List;
 
