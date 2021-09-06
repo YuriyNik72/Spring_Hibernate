b7fa8e7cf9d0db53c11ffb60c35925727d8bbe5a
Yuriy <yurgennik@gmail.com>
2021-09-06 15:55:52 +0500

    Fix3

--- src/main/java/com/nikitin/spring_hibernate/repositories/ProductRepository.java
+++ src/main/java/com/nikitin/spring_hibernate/repositories/ProductRepository.java
@@ -1,9 +1,9 @@
-package com.nikitin.spring_hibernate.repositories;
-
-import com.nikitin.spring_hibernate.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
+//package com.nikitin.spring_hibernate.repositories;
+//
+//import com.nikitin.spring_hibernate.entity.Product;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//
+//public interface ProductRepository extends JpaRepository<Product, Long> {
+//
+//}
